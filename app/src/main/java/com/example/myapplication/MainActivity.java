38586aa146cda811e014b9fca5250c1227cf84b9
farmansunasara <farmansunasara07@gmail.com>
2024-01-16 07:01:17 +0530

    register page add

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView register = (TextView) findViewById(R.id.lnkregister);
+        register.setMovementMethod(LinkMovementMethod.getInstance());
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
