b879bf58c97fb73aa0d0b8a060cc5c30508f6397
farmansunasara <farmansunasara07@gmail.com>
2024-01-17 09:22:31 +0530

    admin page add

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,10 @@ import android.content.Intent;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         TextView register = (TextView) findViewById(R.id.lnkregister);
         register.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView username =(TextView) findViewById(R.id.emaillogin) ;
+        TextView password=(TextView) findViewById(R.id.passwordlogin);
+        MaterialButton loginbtn =(MaterialButton) findViewById(R.id.loginbtn);
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    Toast.makeText(MainActivity.this,"LOGIN Successfull",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,"LOGIN UnSuccessfull",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
