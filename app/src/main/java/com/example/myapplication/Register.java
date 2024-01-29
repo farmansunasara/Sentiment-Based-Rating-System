503f8555b1f48d04e24d07fe63725ea62b73f0b9
farmansunasara <farmansunasara07@gmail.com>
2024-01-29 18:35:23 +0530

    validation

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -24,6 +24,9 @@ import com.google.android.material.button.MaterialButton;
         EditText txtcountry;
         EditText txtpincode;
         MaterialButton btnregister;
+        // one boolean variable to check whether all the text fields
+        // are filled by the user, properly or not.
+        boolean isAllFieldsChecked = false;
 
 
     @Override
@@ -46,7 +49,12 @@ import com.google.android.material.button.MaterialButton;
             @Override
             public void onClick(View v) {
                 MyDatabaseHelper myDB =new MyDatabaseHelper(Register.this);
-                myDB.addCustomer(customernametxt.getText().toString().trim(),Emailtxt.getText().toString().trim(),txtPwd.getText().toString().trim(),txtmobile.getText().toString().trim(), txtAddress.getText().toString().trim(),txtcity.getText().toString().trim(),txtstate.getText().toString().trim(),txtcountry.getText().toString().trim(),Integer.valueOf( txtpincode.getText().toString().trim()));
+                isAllFieldsChecked = CheckAllFields();
+                if(isAllFieldsChecked) {
+                    myDB.addCustomer(customernametxt.getText().toString().trim(), Emailtxt.getText().toString().trim(), txtPwd.getText().toString().trim(), txtmobile.getText().toString().trim(), txtAddress.getText().toString().trim(), txtcity.getText().toString().trim(), txtstate.getText().toString().trim(), txtcountry.getText().toString().trim(), Integer.valueOf(txtpincode.getText().toString().trim()));
+                    Intent intent = new Intent(Register.this, MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -62,5 +70,33 @@ import com.google.android.material.button.MaterialButton;
 
 
     }
+
+        private boolean CheckAllFields() {
+            if (customernametxt.length() == 0) {
+                customernametxt.setError("This field is required");
+                return false;
+            }
+
+            if (Emailtxt.length() == 0) {
+                Emailtxt.setError("This field is required");
+                return false;
+            }
+
+            if (txtPwd.length() == 0) {
+                txtPwd.setError("Email is required");
+                return false;
+            }
+
+            if (txtmobile.length() == 0) {
+                txtmobile.setError("Password is required");
+                return false;
+            } else if (txtmobile.length() < 11) {
+                txtmobile.setError("Password must be minimum 8 characters");
+                return false;
+            }
+
+            // after all validation return true.
+            return true;
+        }
 
 }
