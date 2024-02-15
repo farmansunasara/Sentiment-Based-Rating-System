311448329c1415ce0f4ade516df82c3ec8ff6556
farmansunasara <farmansunasara07@gmail.com>
2024-02-15 13:22:45 +0530

    cart work slider

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -1,30 +1,20 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Environment;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.denzcoskun.imageslider.ImageSlider;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
@@ -32,16 +22,15 @@ public class HomeFragment extends Fragment {
     private RecyclerView categoryRecycler;
     private RecyclerView productRecycler;
 
-    MyDatabaseHelper myDB;
+    private MyDatabaseHelper myDB;
     private UserCategoryAdaptor userCategoryAdaptor;
     private UserProductAdaptor userProductAdaptor;
-    private ImageSlider imageSlider;
+    private ImageSlider image_slider_banner;
 
-    ArrayList<String> categoryIds, categoryNames;
-    ArrayList<byte[]> categoryImages;
-    ArrayList<String> productIds, productNames, productDescriptions, productMRPs, productSPs, productcategory;
-
-    ArrayList<byte[]> productcov_img, productselected_img;
+    private ArrayList<String> categoryIds, categoryNames;
+    private ArrayList<byte[]> categoryImages;
+    private ArrayList<String> productIds, productNames, productDescriptions, productMRPs, productSPs, productcategory;
+    private ArrayList<byte[]> productcov_img, productselected_img;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -49,11 +38,13 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
         myDB = new MyDatabaseHelper(getActivity());
-        categoryRecycler =rootView.findViewById(R.id.categoryRecycler);
-        productRecycler=rootView.findViewById(R.id.productRecycler);
+        categoryRecycler = rootView.findViewById(R.id.categoryRecycler);
+        productRecycler = rootView.findViewById(R.id.productRecycler);
+        image_slider_banner = rootView.findViewById(R.id.image_slider_banner); // Initializing ImageSlider
+
         categoryIds = new ArrayList<>();
         categoryNames = new ArrayList<>();
-        categoryImages = new ArrayList<byte[]>();
+        categoryImages = new ArrayList<>();
         productIds = new ArrayList<>();
         productNames = new ArrayList<>();
         productDescriptions = new ArrayList<>();
@@ -63,67 +54,43 @@ public class HomeFragment extends Fragment {
         productcov_img = new ArrayList<>();
         productselected_img = new ArrayList<>();
 
+        retrieveSliderImagePaths();
+
         try {
             storeDataInArrays();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         try {
             storeDataInArraysproducts();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         setupRecyclerView();
 
         return rootView;
     }
-    private void storeDataInArrays() throws IOException {
+
+    private void storeDataInArrays() {
         Cursor cursor = myDB.viewCategoryDetails();
         if (cursor != null && cursor.getCount() > 0) {
             int imagePathColumnIndex = cursor.getColumnIndex(MyDatabaseHelper.CATEGORY_IMG_PATH);
-
-            if (imagePathColumnIndex != -1) {
-                while (cursor.moveToNext()) {
-                    categoryIds.add(cursor.getString(0));
-                    categoryNames.add(cursor.getString(1));
-
-                    String imagePath = cursor.getString(imagePathColumnIndex);
-
-                    if (imagePath != null) {
-                        Log.d("Image File Path", imagePath);
-                        File f = new File(imagePath);
-
-                        if (f.exists()) {
-                            Bitmap myBitmap= BitmapFactory.decodeFile(imagePath);
-                            ByteArrayOutputStream stream=new ByteArrayOutputStream();
-                            myBitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
-                            byte[] bytes=stream.toByteArray();
-
-                            categoryImages.add(bytes);
-
-                            Log.d("Image File Size", String.valueOf(f.length()));
-                        } else {
-                            Log.e("Image File", "File does not exist: " + imagePath);
-                        }
-                    } else {
-                        Log.e("Image File", "Image path is null");
-                    }
-                }
+            while (cursor.moveToNext()) {
+                categoryIds.add(cursor.getString(0));
+                categoryNames.add(cursor.getString(1));
+                byte[] imageBytes = cursor.getBlob(imagePathColumnIndex);
+                categoryImages.add(imageBytes);
             }
+            cursor.close();
         }
-
-
     }
 
-
-    void storeDataInArraysproducts() throws IOException {
+    private void storeDataInArraysproducts() {
         Cursor cursor = myDB.viewProductDetails();
-
         if (cursor != null && cursor.getCount() > 0) {
             int coverImagePathColumnIndex = cursor.getColumnIndex(MyDatabaseHelper.PRODUCT_COVER_IMAGE);
-
             while (cursor.moveToNext()) {
                 productIds.add(cursor.getString(0));
                 productNames.add(cursor.getString(1));
@@ -131,58 +98,70 @@ public class HomeFragment extends Fragment {
                 productMRPs.add(cursor.getString(3));
                 productSPs.add(cursor.getString(4));
                 productcategory.add(cursor.getString(5));
-
-                // For cover image
-                if (coverImagePathColumnIndex != -1) {
-                    String coverImagePath = cursor.getString(coverImagePathColumnIndex);
-                    if (coverImagePath != null) {
-                        handleImage(coverImagePath, productcov_img);
-                    }
-                }
-
-                // For selected images
-//                for (int i = 0; i < MAX_PRODUCT_IMAGES; i++) {
-//                    String imagePathColumnName = MyDatabaseHelper.PRODUCT_IMAGE + "_" + i;
-//                    int imagePathColumnIndexForSelectedImages = cursor.getColumnIndex(imagePathColumnName);
-//
-//                    if (imagePathColumnIndexForSelectedImages != -1) {
-//                        String imagePath = cursor.getString(imagePathColumnIndexForSelectedImages);
-//                        if (imagePath != null) {
-//                            handleImage(imagePath, productselected_img);
-//                        }
-//                    }
-//                }
+                byte[] coverImageBytes = cursor.getBlob(coverImagePathColumnIndex);
+                productcov_img.add(coverImageBytes);
             }
+            cursor.close();
         } else {
             Toast.makeText(getActivity(), "No data", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void handleImage(String imagePath, ArrayList<byte[]> imageList) {
-        File f = new File(imagePath);
-        if (f.exists()) {
-            Bitmap myBitmap = BitmapFactory.decodeFile(imagePath);
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            myBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            byte[] bytes = stream.toByteArray();
-            imageList.add(bytes);
-            Log.d("Image File Size", String.valueOf(f.length()));
+//    private void retrieveSliderImagePaths() {
+//        // Add a static image path to the sliderImageList
+//        ArrayList<SlideModel> sliderImageList = new ArrayList<>();
+//        sliderImageList.add(new SlideModel("/storage/emulated/0/iotdirectory/slider170793482149920240214_235021.jpg", null));
+//
+//        // Check if sliderImageList is not empty before setting it
+//        if (!sliderImageList.isEmpty()) {
+//            image_slider_banner.setImageList(sliderImageList);
+//        } else {
+//            // Log a warning if no images were found
+//            Log.w("Slider Image Path", "No images found in database");
+//        }
+//    }
+
+    private void retrieveSliderImagePaths() {
+        Cursor cursor = myDB.viewSliderDetails();
+        if (cursor != null && cursor.getCount() > 0) {
+            ArrayList<SlideModel> sliderImageList = new ArrayList<>();
+            int imagePathColumnIndex = cursor.getColumnIndex(MyDatabaseHelper.SLIDER_IMAGE);
+            while (cursor.moveToNext()) {
+                String imagePath = cursor.getString(imagePathColumnIndex);
+                if (imagePath != null) {
+                    // Log the retrieved image path
+                    Log.d("Slider Image Path", imagePath);
+                    // Create SlideModel object and add to the list
+                    sliderImageList.add(new SlideModel(imagePath, null));
+                }
+            }
+            // Check if sliderImageList is not empty before setting it
+            Log.d("Slider Image List", "Size: " + sliderImageList.size());
+            for (SlideModel model : sliderImageList) {
+                Log.d("Slider Image List", "Image Path: " + model.getImageUrl());
+            }
+            if (!sliderImageList.isEmpty()) {
+               image_slider_banner.setImageList(sliderImageList);
+            } else {
+                // Log a warning if no images were found
+                Log.w("Slider Image Path", "No images found in database");
+            }
         } else {
-            Log.e("Image File", "File does not exist: " + imagePath);
+            // Log an error if cursor is null or empty
+            Log.e("Slider Image Path", "No slider images found in database");
+        }
+        // Close the cursor
+        if (cursor != null) {
+            cursor.close();
         }
     }
-
     private void setupRecyclerView() {
-        userCategoryAdaptor= new UserCategoryAdaptor(getActivity(), categoryIds, categoryNames, categoryImages);
+        userCategoryAdaptor = new UserCategoryAdaptor(getActivity(), categoryIds, categoryNames, categoryImages);
         categoryRecycler.setAdapter(userCategoryAdaptor);
         categoryRecycler.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
 
-        userProductAdaptor = new UserProductAdaptor(getActivity(),myDB, productIds, productNames, productDescriptions, productMRPs, productSPs, productcategory, productcov_img, productselected_img);
+        userProductAdaptor = new UserProductAdaptor(getActivity(), myDB, productIds, productNames, productDescriptions, productMRPs, productSPs, productcategory, productcov_img, productselected_img);
         productRecycler.setAdapter(userProductAdaptor);
         productRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
-
-
-
-
 }
